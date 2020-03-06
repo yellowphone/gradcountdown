2f628ace9a7e8d342d3188dae4dc1a0c271b613c
geomin76 <root@DESKTOP-L1S1A71.localdomain>
2020-03-05 19:23:14 -0500

    twitter code

--- README.md
+++ README.md
@@ -8,3 +8,19 @@ How to run:
 - run `npm install`
 - run `npm start` (if you don't have nodejs installed, please install)
 - Open a browser and go to "http://localhost:8080"
+
+Twitter config:
+- Create a file called `config.js` in the gradcountdown repository
+- Inside `config.js`, put this inside the file:
+~~~
+module.exports = {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+}
+~~~
+- Go to https://tweeterbot-270223.appspot.com , create a Twitter project and get your keys and secrets! 
+- REMEMBER: NEVER GIVE OUT YOUR KEYS AND SECRETS
+- Fill out config.js, and run `npm start`. 
+- Inside app.js, you can config whatever tweet you want to send!
--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+var schedule = require('node-schedule');
+var config = require('./config.js');
+var Twitter = require('twitter');
+var client = new Twitter(config);
+
+var j = schedule.scheduleJob('00 30 19 * * *', function() {
+    let date_ob = new Date();
+    console.log("test every day");
+    let date = ("0", date_ob.getDate())
+    let month = ("0" + (date_ob.getMonth() + 1))
+    console.log(month + "/" + date + " " + date_ob.getHours() + ":" + date_ob.getMinutes() + ":" + date_ob.getSeconds())
+    client.post('statuses/update', {status: "Good morning, and in case I don't see ya, good afternoon, good evening, and good night!"}, function(error, tweet, response) {
+        if (error) throw error;
+        console.log(tweet);
+        console.log(response);
+    })
+})
 
 app.get('/', (req, res) => {
-    res
-        .status(200)
-        .send('Hello, world!')
-        .end();
+    res.send("Hello, World!")
+    res.end();
 })
 
 const PORT = process.env.PORT || 8080;
